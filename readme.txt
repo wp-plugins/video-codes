29bcb906a01a4da95684981cd2ac7ededc128219
ibabar <ibabar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-15 23:51:36 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/video-codes/trunk@423992 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,64 @@
+===Video Codes===
+Contributors: ibabar
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=UF6NETKA8S7S6
+Tags: Video, embed, easy video embed, emved video easily, Youtube, Google video, Metacafe, Megavideo, Vimeo, Facebook, Facebook Videos, Video embed shortcodes, Shortcodes, Videos Codes
+Requires at least: 3.0
+Tested Up to: 3.1.2
+Stable Tag: 1.0
+
+The easiest way to embed videos on your Wordpress Site and,or blog.
+
+==Description==
+Bored about inserting video embed codes on HTML mode and when back all are messed? Searcing for a better solution?
+Yap, you have just hitted on the right place! Withh Video Codes now Embed a video is as easy as type your name!
+Now you don't have to be a geek to insert a video on your blog posts. It's just Get, Set and Go!
+
+Supported Hosts:
+<ol>
+<li>Youtube</li>
+<li>Google Video</li>
+<li>DailyMotion</li>
+<li>MetaCafe</li>
+<li>MegaVideo</li>
+<li>Facebook</li>
+</ol>
+(Adding more hosts. Let me know if you have any special choice or need)
+
+Still searcing for something? Just drop e a message @ <a href="http://en.ibabar.com" title="Use the contact form on my blog" rel="follow">My Blog</a>
+
+== Installation ==
+You can Install it in two easy ways.
+Go to your Wordpress Dashboard plugin section, upload and install.
+or another way,
+Put the videocodes.php file in your Plugins folder.
+Now go to yor dashboard plugin section and active Video Codes.
+Isn't it simple!?
+
+== Frequently Asked Questions ==
+Coming Soon.
+
+== Upgrade Notice ==
+Coming Soon.
+
+== Screenshots ==
+Coming Soon.
+
+==User Manual==
+For all hosts the common format is: [consernig_host_name id=VIDEO_ID]
+<ul>
+<li>Like for Youtube: [vcyt id=VIDEO_ID]</li>
+<li>Google Video: [vcgv id=VIDEO_ID]</li>
+<li>DailyMotion: [vcdm id=VIDEO_ID]</li>
+<li>MetaCafe: [vcmc id=VIDEO_ID]</li>
+<li>MegaVideo: [vcmv id=VIDEO_ID]</li>
+<li>Facebook: [vcfb id=VIDEO_ID]</li>
+</ul>
+You can change the width and height easily with 'w' and 'h' parametre.<br />
+For example: [vcyt id=VIDEO_ID w=640 h=385]
+
+Have further question? Let me know.
+
+==Changelog==
+Date: 12-08-2011
+Released Version: 1.0
+Release Note: Fist Release of Video Codes. Launching with 6 Video sharing site support.
---
+++ video-codes.php
@@ -0,0 +1,108 @@
+<?php
+/*Plugin Name: Video Codes
+Plugin URI: http://www.workerslab.com
+Description: The easiest way to add youtube videos on your Wordpress Blog.
+Version: 1.0
+Author: Babar
+Author URI: http://www.iBabar.com
+License: GPL v2
+*/
+
+/*Youtube Video ShortCode*/
+function yt_shortcode( $atts ) {
+   extract( shortcode_atts( array(
+      'h' => '385',
+      'w' => '640',
+      'id' => null,
+      'rel' => null,
+      'ytlogo' => null,
+      'theme' => null,
+      'info' => null,
+      'playlist' => null,
+      'time' => '0',
+      'border' => '0',
+      ), $atts ) );
+ 
+   return '<iframe class="youtube-player" type="text/html" width="' . esc_attr($w) . '" height="' . esc_attr($h) . '" src="http:
+//www.youtube.com/embed/' . esc_attr($id) . '/?modestbranding=' . esc_attr($ytlogo) . '&amp;theme=' . esc_attr($theme) . '&amp;autohide=1&amp;rel=' . esc_attr($rel) . '&amp;showinfo=' . esc_attr($info) . 'playlist=' . esc_attr($playlist) . '&amp;start=' . esc_attr($time) . '" frameborder="' . esc_attr($border) . '" ></iframe>';
+}
+add_shortcode( 'yt', 'yt_shortcode' );
+
+/*Google Video ShortCote*/
+function vcgv_shortcode( $atts ) {
+   extract( shortcode_atts( array(
+      'h' => '326',
+      'w' => '400',
+      'id' => null,
+      ), $atts ) );
+ 
+   return '<embed id=VideoPlayback src=http://video.google.com/googleplayer.swf?docid=' . esc_attr($id) . '&amp;hl=en&amp;fs=true style=width:' . esc_attr($w) . 'px;height:' . esc_attr($h) . 'px allowFullScreen=true allowScriptAccess=always type=application/x-shockwave-flash> </embed>';
+}
+add_shortcode( 'vcgv', 'vcgv_shortcode' );
+
+/*Vimeo Video Shortcode*/
+function vcvv_shortcode( $atts ) {
+   extract( shortcode_atts( array(
+      'h' => '338',
+      'w' => '600',
+      'id' => null,
+      'title' => '0',
+      'byline' => '0',
+      'portrait' => '0',
+      'border' => '0',
+      ), $atts ) );
+ 
+   return '<iframe src="http://player.vimeo.com/video/' . esc_attr($id) . '?title=' . esc_attr($title) . '&amp;byline=' . esc_attr($byline) . '&amp;portrait=' . esc_attr($portrait) . '" width="' . esc_attr($w) . '" height="' . esc_attr($h) . '" frameborder="' . esc_attr($border) . '"></iframe>';
+}
+add_shortcode( 'vcvv', 'vcvv_shortcode' );
+
+/*DailyMotion Video ShortCode*/
+function vcdm_shortcode( $atts ) {
+   extract( shortcode_atts( array(
+      'h' => '332',
+      'w' => '480',
+      'id' => null,
+      'border' => '0',
+      ), $atts ) );
+ 
+   return '<iframe frameborder="' . esc_attr($border) . '" width="' . esc_attr($w) . '" height="' . esc_attr($h) . '" src="http://www.dailymotion.com/embed/video/' . esc_attr($id) . '"></iframe>';
+}
+add_shortcode( 'vcdm', 'vcdm_shortcode' );
+
+
+/*MetaCafe Video ShortCode*/
+function vcmc_shortcode( $atts ) {
+   extract( shortcode_atts( array(
+      'h' => '370',
+      'w' => '600',
+      'id' => null,
+      ), $atts ) );
+ 
+   return '<object type="application/x-shockwave-flash" data="http://www.metacafe.com/fplayer/' . esc_attr($id) . '/.swf" width="' . esc_attr($w) . '" height="' . esc_attr($h) . '" wmode="transparent"><param name="movie" value="http://www.metacafe.com/fplayer/' . esc_attr($id) . '/.swf" /></object>';
+}
+add_shortcode( 'vcmc', 'vcmc_shortcode' );
+
+
+/*MegaVideo ShortCode*/
+function vcmv_shortcode( $atts ) {
+   extract( shortcode_atts( array(
+      'h' => '344',
+      'w' => '640',
+      'id' => null,
+      ), $atts ) );
+ 
+   return '<object width="' . esc_attr($w) . '" height="' . esc_attr($h) . '"><param name="movie" value="http://www.megavideo.com/v/' . esc_attr($id) . '.3920544471.0"></param><param name="allowFullScreen" value="true"></param><embed src="http://www.megavideo.com/v/' . esc_attr($id) . '.3920544471.0" type="application/x-shockwave-flash" allowfullscreen="true" width="' . esc_attr($w) . '" height="' . esc_attr($h) . '"></embed></object>';
+}
+add_shortcode( 'vcmv', 'vcmv_shortcode' );
+
+/*Facebook Video ShortCode*/
+function vcfb_shortcode( $atts ) {
+   extract( shortcode_atts( array(
+      'h' => '344',
+      'w' => '640',
+      'id' => null,
+      ), $atts ) );
+ 
+   return '<object width="' . esc_attr($w) . '" height="' . esc_attr($h) . '"><param name="movie" value="http://www.facebook.com/v/' . esc_attr($id) . '"></param><param name="allowFullScreen" value="true"></param><embed src="http://www.megavideo.com/v/' . esc_attr($id) . '" type="application/x-shockwave-flash" allowfullscreen="true" width="' . esc_attr($w) . '" height="' . esc_attr($h) . '"></embed></object>';
+}
+add_shortcode( 'vfbv', 'vcfb_shortcode' );
